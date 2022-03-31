458c58420de32946baf2ba1c19e3a5c36ece66b0
Víctor Alonso <works.alonsog@gmail.com>
2022-03-31 14:08:04 +0200

    update

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,11 +6,19 @@ function calcMenuWidth(resize) {
 		var $menuItem = $('.js-submenu-dir-category');
 		var $viewMore = $('.js-submenu-dir-more');
 		var $inputWhere = $('.js-submenu-input-where');
+		var $menuList = $('.js-submenu-dir-list');
 
+		var listWidth = $menuList.outerWidth(true);
 		var moreWidth = $viewMore.outerWidth(true);
 		var inputWhereWidth = $inputWhere.outerWidth(true);
 		var availableSpace = $menu.width() - moreWidth - inputWhereWidth;
 		var navWidth = 0;
+
+		if($menu.width() < (listWidth + inputWhereWidth)){
+			$($viewMore).removeClass('is__hidden');
+		}else{
+			$($viewMore).addClass('is__hidden');
+		}
 
 		$menuItem.each(function() {
 			navWidth += $(this).outerWidth(true);
