ed2a73440b4cd3e2ba005d35e80d51460fa57957
Víctor Alonso <works.alonsog@gmail.com>
2022-03-31 13:44:11 +0200

    commit inicial

---
+++ assets/js/search.js
@@ -0,0 +1,31 @@
+function calcMenuWidth(resize) {
+	var bodyWidth = $("body").width();
+
+	if (bodyWidth > 1024) {
+		var $menu = $('.js-submenu-dir');
+		var $menuItem = $('.js-submenu-dir-category');
+		var $viewMore = $('.js-submenu-dir-more');
+		var $inputWhere = $('.js-submenu-input-where');
+
+		var moreWidth = $viewMore.outerWidth(true);
+		var inputWhereWidth = $inputWhere.outerWidth(true);
+		var availableSpace = $menu.width() - moreWidth - inputWhereWidth;
+		var navWidth = 0;
+
+		$menuItem.each(function() {
+			navWidth += $(this).outerWidth(true);
+			
+			if ((navWidth < availableSpace) && resize !== null) {
+				$(this).removeClass('is__hidden');
+			} else {
+				$(this).addClass('is__hidden');
+			}
+		});
+	}
+}
+
+$(window).on('resize', function(e) {
+	calcMenuWidth(e);
+});
+
+calcMenuWidth();
